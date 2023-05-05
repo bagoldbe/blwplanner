e6c9be1c74255bec83627af8e6934f0649d6b046
Ben Goldberg <ben@goby.co>
2023-05-05 14:51:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ function getMealPlan() {
   const menu = [
     {
       name: "Scrambled eggs with cheese",
-      ingredients: ["eggs", "cheddar cheese", "butter", "salt", "pepper"],
+      ingredients: ["eggs", "cheddar cheese", "butter"],
     },
     {
       name: "Oatmeal with banana and cinnamon",
-      ingredients: ["rolled oats", "milk", "water", "banana", "cinnamon"],
+      ingredients: ["rolled oats", "milk", "banana"],
     },
     {
       name: "Grilled cheese sandwich",
@@ -22,66 +22,68 @@ function getMealPlan() {
     },
     {
       name: "Sweet potato and black bean tacos",
-      ingredients: [
-        "sweet potato",
-        "black beans",
-        "taco seasoning",
-        "tortillas",
-        "avocado",
-        "lime",
-      ],
+      ingredients: ["sweet potato", "black beans", "taco seasoning"],
     },
     {
       name: "Chicken and vegetable stir fry",
-      ingredients: [
-        "chicken breast",
-        "broccoli",
-        "red bell pepper",
-        "carrots",
-        "onion",
-        "garlic",
-        "soy sauce",
-        "brown sugar",
-        "cornstarch",
-        "rice",
-      ],
+      ingredients: ["chicken breast", "broccoli", "soy sauce"],
     },
     {
       name: "Salmon and vegetable kebabs",
-      ingredients: [
-        "salmon fillet",
-        "zucchini",
-        "red onion",
-        "red bell pepper",
-        "olive oil",
-        "lemon",
-        "dill",
-        "garlic",
-      ],
+      ingredients: ["salmon fillet", "zucchini", "red onion"],
     },
     {
       name: "Mini turkey meatballs with tomato sauce",
-      ingredients: [
-        "ground turkey",
-        "breadcrumbs",
-        "egg",
-        "parmesan cheese",
-        "onion",
-        "garlic",
-        "crushed tomatoes",
-        "tomato paste",
-        "oregano",
-        "pasta",
-      ],
+      ingredients: ["ground turkey", "breadcrumbs", "tomato sauce"],
     },
     {
       name: "Broccoli and cheddar cheese omelet",
-      ingredients: [
-        "eggs",
-        "cheddar cheese",
-        "broccoli",
-        "butter",
-        "salt",
-        "pepper",
-      ],
-   
+      ingredients: ["eggs", "cheddar cheese", "broccoli"],
+    },
+    {
+      name: "Zucchini and corn fritters",
+      ingredients: ["zucchini", "corn", "flour"],
+    },
+    {
+      name: "Spinach and ricotta stuffed shells",
+      ingredients: ["jumbo pasta shells", "ricotta cheese", "spinach"],
+    },
+    {
+      name: "Pesto pasta with cherry tomatoes",
+      ingredients: ["pasta", "basil pesto", "cherry tomatoes"],
+    },
+    {
+      name: "Cauliflower and cheese soup",
+      ingredients: ["cauliflower", "cheddar cheese", "milk"],
+    },
+    {
+      name: "Hummus and vegetable wrap",
+      ingredients: ["hummus", "tortilla", "vegetables"],
+    },
+    {
+      name: "Cheesy cauliflower tots",
+      ingredients: ["cauliflower", "cheddar cheese", "breadcrumbs"],
+    },
+    {
+      name: "Butternut squash and apple soup",
+      ingredients: ["butternut squash", "apple", "onion"],
+    },
+    {
+      name: "Tuna and vegetable sushi rolls",
+      ingredients: ["sushi rice", "tuna", "vegetables"],
+    },
+    {
+      name: "Peanut butter and banana sandwich",
+      ingredients: ["bread", "peanut butter", "banana"],
+    },
+    {
+      name: "Vegetable and lentil curry",
+      ingredients: ["lentils", "vegetables", "curry powder"],
+    },
+    {
+      name: "Sweet potato fries with dipping sauce",
+      ingredients: ["sweet potato", "cornstarch", "dipping sauce"],
+    }
+  ];
+
+  //

7ce9f57406ccc6fb45eb855292c75435d81a1b55
Ben Goldberg <ben@goby.co>
2023-05-05 14:28:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,70 +1,88 @@
-// Get a reference to the button and table elements.
-const generateMealPlanButton = document.getElementById("generateMealPlanButton");
-const mealPlanTable = document.getElementById("meal-plan-table");
-
-const clientId = '745981578491-nv66g7p1d0n1acr86u55rga2cpk0gon5.apps.googleusercontent.com';
-const apiKey = 'AIzaSyBH-ccnmdnoUCSlOjkjYg0Xyyi0CR-y800';
-const spreadsheetId = '1dV087gGD2-eV0Yd8Cr6O6w9qC0mwq8pK48MKdEViAns';
-const range = 'Sheet1!A:B'; // Adjust this range based on the location of your menu items in the sheet
-
-// Define a function to load the Google Sheets API
-async function loadSheetsApi() {
-  const iframe = document.querySelector('iframe[src*="https://apis.google.com"]');
-  if (iframe) {
-    iframe.removeAttribute('sandbox');
-  }
-
-  await new Promise((resolve) => setTimeout(resolve, 100));
-
-  gapi.load('client:auth2', initClient);
-}
-
-
-
-// Initialize the Google Sheets API client
-function initClient() {
-  console.log("Google Sheets API loaded successfully.");
-  gapi.client.init({
-    apiKey: apiKey,
-    clientId: clientId,
-    scope: 'https://www.googleapis.com/auth/spreadsheets.readonly',
-    cookie_policy: 'none'
-  }).then(() => {
-    console.log("Google Sheets API client initialized successfully.");
-    gapi.auth2.getAuthInstance().signIn();
-  }).catch((error) => {
-    console.error("Failed to initialize Google Sheets API client: ", error);
-  });
-}
-
-// Fetch menu items from Google Sheet
-function fetchMenuItems() {
-  if (!gapi.client) {
-    throw new Error('Google Sheets API client not initialized.');
-  }
-  
-  return gapi.client.sheets.spreadsheets.values.get({
-    spreadsheetId: spreadsheetId,
-    range: range,
-  }).then((response) => {
-    const menu = response.result.values.flat();
-    return menu;
-  });
-}
-
-
-// Define a function to generate the meal plan.
-async function createMealPlan() {
+// Define a function to generate the meal plan
+function createMealPlan() {
   const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
   const meals = ["Breakfast", "Lunch", "Dinner"];
   const allergens = ["cow's milk", "egg", "finned fish", "peanut", "sesame", "shellfish", "soy", "tree nuts", "wheat"];
-  const menu = await fetchMenuItems();
   
-  // (rest of the code remains the same)
+  // Define a variable to hold the meal plan
+  const mealPlan = {};
+
+  // Loop through each day
+  for (const day of days) {
+    // Add an object for the day to the mealPlan object
+    mealPlan[day] = {};
+
+    // Loop through each meal
+    for (const meal of meals) {
+      // Add an array for the meal to the day object
+      mealPlan[day][meal] = [];
+
+      // Loop through each allergen
+      for (const allergen of allergens) {
+        // Generate a random boolean value for each allergen
+        const hasAllergen = Math.random() < 0.1;
+
+        // If the meal contains the allergen, add it to the meal array
+        if (hasAllergen) {
+          mealPlan[day][meal].push(allergen);
+        }
+      }
+    }
+  }
+
+  // Store the meal plan in localStorage
+  localStorage.setItem("mealPlan", JSON.stringify(mealPlan));
 }
 
-// Add an event listener to the button.
-generateMealPlanButton.addEventListener("click", createMealPlan);
+// Define a function to retrieve the meal plan from localStorage
+function getMealPlan() {
+  // Get the meal plan from localStorage
+  const mealPlanJSON = localStorage.getItem("mealPlan");
 
-// Load the Google Sheets API
-loadSheetsApi();
+  // If the meal plan is not found in localStorage, return an empty object
+  if (!mealPlanJSON) {
+    return {};
+  }
+
+  // Parse the JSON string and return the meal plan object
+  return JSON.parse(mealPlanJSON);
+}
+
+// Define a function to display the meal plan in the HTML table
+function displayMealPlan() {
+  const mealPlan = getMealPlan();
+  const mealPlanTableBody = document.getElementById("meal-plan-table-body");
+  
+  // Loop through each day
+  for (const day in mealPlan) {
+    // Loop through each meal
+    for (const meal in mealPlan[day]) {
+      // Create a table row for the meal
+      const row = mealPlanTableBody.insertRow();
+
+      // Add the day to the first cell
+      const dayCell = row.insertCell();
+      dayCell.textContent = day;
+
+      // Add the meal to the second cell
+      const mealCell = row.insertCell();
+      mealCell.textContent = meal;
+
+      // Add the allergens to the third cell
+      const allergensCell = row.insertCell();
+      allergensCell.textContent = mealPlan[day][meal].join(", ");
+    }
+  }
+}
+
+// Add an event listener to the button to generate the meal plan and display it in the table
+const generateMealPlanButton = document.getElementById("generateMealPlanButton");
+generateMealPlanButton.addEventListener("click", () => {
+  createMealPlan();
+  displayMealPlan();
+});
+
+// Display the meal plan when the page is loaded
+window.addEventListener("load", () => {
+  displayMealPlan();
+});

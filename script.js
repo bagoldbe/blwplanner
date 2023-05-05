88789125c32a770e2d59e857576dab1925ece2bb
Ben Goldberg <ben@goby.co>
2023-05-05 14:45:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,121 @@
 // Get a reference to the button and table elements.
 const generateMealPlanButton = document.getElementById("generateMealPlanButton");
 const mealPlanTable = document.getElementById("meal-plan-table");
-const recipeElement = document.getElementById("recipe");
 
 // Define a function to generate the meal plan.
 function getMealPlan() {
   const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
   const meals = ["Breakfast", "Lunch", "Dinner"];
   const allergens = ["cow's milk", "egg", "finned fish", "peanut", "sesame", "shellfish", "soy", "tree nuts", "wheat"];
-  const menu = [    "Scrambled eggs with cheese",    "Oatmeal with banana and cinnamon",    "Grilled cheese sandwich",    "Sweet potato and black bean tacos",    "Chicken and vegetable stir fry",    "Salmon and vegetable kebabs",    "Mini turkey meatballs with tomato sauce",    "Broccoli and cheddar cheese omelet",    "Zucchini and corn fritters",    "Spinach and ricotta stuffed shells",    "Pesto pasta with cherry tomatoes",    "Cauliflower and cheese soup",    "Quinoa and black bean burgers",    "Hummus and vegetable wrap",    "Cheesy cauliflower tots",    "Butternut squash and apple soup",    "Tuna and vegetable sushi rolls",    "Peanut butter and banana sandwich",    "Vegetable and lentil curry",    "Sweet potato fries with dipping sauce"  ];
-
-  // Generate the meal plan.
-  const mealPlan = {};
-  days.forEach((day) => {
-    mealPlan[day] = {};
-    meals.forEach((meal) => {
-      const menuIndex = Math.floor(Math.random() * menu.length);
-      const menuItem = menu[menuIndex];
-      mealPlan[day][meal] = menuItem;
-    });
-    mealPlan[day]["Allergens"] = allergens[Math.floor(Math.random() * allergens.length)];
-  });
-
-  // Store the meal plan in Local Storage.
-  localStorage.setItem("mealPlan",
+  const menu = [    {      name: "Scrambled eggs with cheese",      ingredients: ["eggs", "cheddar cheese", "butter", "salt", "pepper"],
+    },
+    {
+      name: "Oatmeal with banana and cinnamon",
+      ingredients: ["rolled oats", "milk", "water", "banana", "cinnamon"],
+    },
+    {
+      name: "Grilled cheese sandwich",
+      ingredients: ["bread", "cheddar cheese", "butter"],
+    },
+    {
+      name: "Sweet potato and black bean tacos",
+      ingredients: [
+        "sweet potato",
+        "black beans",
+        "taco seasoning",
+        "tortillas",
+        "avocado",
+        "lime",
+      ],
+    },
+    {
+      name: "Chicken and vegetable stir fry",
+      ingredients: [
+        "chicken breast",
+        "broccoli",
+        "red bell pepper",
+        "carrots",
+        "onion",
+        "garlic",
+        "soy sauce",
+        "brown sugar",
+        "cornstarch",
+        "rice",
+      ],
+    },
+    {
+      name: "Salmon and vegetable kebabs",
+      ingredients: [
+        "salmon fillet",
+        "zucchini",
+        "red onion",
+        "red bell pepper",
+        "olive oil",
+        "lemon",
+        "dill",
+        "garlic",
+      ],
+    },
+    {
+      name: "Mini turkey meatballs with tomato sauce",
+      ingredients: [
+        "ground turkey",
+        "breadcrumbs",
+        "egg",
+        "parmesan cheese",
+        "onion",
+        "garlic",
+        "crushed tomatoes",
+        "tomato paste",
+        "oregano",
+        "pasta",
+      ],
+    },
+    {
+      name: "Broccoli and cheddar cheese omelet",
+      ingredients: [
+        "eggs",
+        "cheddar cheese",
+        "broccoli",
+        "butter",
+        "salt",
+        "pepper",
+      ],
+    },
+    {
+      name: "Zucchini and corn fritters",
+      ingredients: [
+        "zucchini",
+        "corn",
+        "flour",
+        "baking powder",
+        "eggs",
+        "milk",
+        "green onions",
+        "sour cream",
+      ],
+    },
+    {
+      name: "Spinach and ricotta stuffed shells",
+      ingredients: [
+        "jumbo pasta shells",
+        "ricotta cheese",
+        "spinach",
+        "garlic",
+        "parmesan cheese",
+        "marinara sauce",
+      ],
+    },
+    {
+      name: "Pesto pasta with cherry tomatoes",
+      ingredients: [
+        "pasta",
+        "basil pesto",
+        "cherry tomatoes",
+        "parmesan cheese",
+        "olive oil",
+        "garlic",
+      ],
+    },
+    {
+      name: "

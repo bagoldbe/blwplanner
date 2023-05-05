7ef9b196f78842d65fe01143c152887e07b7446c
Ben Goldberg <ben@goby.co>
2023-05-05 11:16:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
+// Get a reference to the button and table elements.
+const generateMealPlanButton = document.getElementById("generateMealPlanButton");
+const mealPlanTable = document.getElementById("meal-plan-table");
+
+// Define a function to generate the meal plan.
 function generateMealPlan() {
-  var safeFoods = [
-    "Banana", "Avocado", "Oatmeal", "Sweet potato", "Butternut squash", "Chicken",
-    "Salmon", "Tofu", "Eggs", "Yogurt", "Beef", "Lentils", "Quinoa", "Brown rice",
-    "Pasta", "Peanut butter", "Bread", "Apples", "Oranges", "Berries", "Green beans",
-    "Carrots", "Zucchini", "Squash", "Peas", "Potatoes", "Tomatoes", "Onions",
-    "Garlic", "Cucumbers", "Celery", "Kale", "Spinach", "Broccoli", "Cauliflower",
-    "Sweet potatoes", "Pumpkin", "Watermelon", "Cantaloupe", "Honeydew", "Grapefruit",
-    "Mango", "Papaya", "Apricots", "Peaches", "Nectarines"
-  ];
-  var allergens = ["Peanuts", "Tree nuts", "Eggs", "Milk", "Fish", "Shellfish", "Soy", "Wheat"];
-  var mealPlanTable = document.getElementById("meal-plan-table").getElementsByTagName('tbody')[0];
-  mealPlanTable.innerHTML = '';
-  for (var day = 0; day < 7; day++) {
-    var breakfast = safeFoods[Math.floor(Math.random() * safeFoods.length)];
-    var lunch = safeFoods[Math.floor(Math.random() * safeFoods.length)];
-    var dinner = safeFoods[Math.floor(Math.random() * safeFoods.length)];
-    var allergensOffered = [];
+  // Get the list of safe foods for babies of all ages.
+  const safeFoods = ["Banana", "Avocado", "Oatmeal", "Sweet potato", "Butternut squash", "Chicken", "Salmon", "Tofu", "Eggs", "Yogurt", "Beef", "Lentils", "Quinoa", "Brown rice", "Pasta", "Peanut butter", "Bread", "Apples", "Oranges", "Bananas", "Berries", "Green beans", "Carrots", "Zucchini", "Squash", "Peas", "Potatoes", "Tomatoes", "Onions", "Garlic", "Cucumbers", "Celery", "Kale", "Spinach", "Broccoli", "Cauliflower", "Sweet potatoes", "Pumpkin", "Watermelon", "Cantaloupe", "Honeydew", "Grapefruit", "Mango", "Papaya", "Apricots", "Peaches", "Nectarines"];
+  
+  // Create a list of common allergens.
+  const allergens = ["Peanuts", "Tree nuts", "Eggs", "Milk", "Fish", "Shellfish", "Soy", "Wheat"];
+  
+  // Create a weekly meal plan.
+  const mealPlan = [];
+  
+  for (let day = 0; day < 7; day++) {
+    // Get a random food from the list of safe foods.
+    const breakfast = safeFoods[Math.floor(Math.random() * safeFoods.length)];
+    const lunch = safeFoods[Math.floor(Math.random() * safeFoods.length)];
+    const dinner = safeFoods[Math.floor(Math.random() * safeFoods.length)];
+    
+    // If the food is an allergen, add it to the list of allergens offered that day.
+    const allergensOffered = [];
     if (allergens.includes(breakfast)) {
       allergensOffered.push(breakfast);
     }
@@ -25,11 +30,19 @@ function generateMealPlan() {
     if (allergens.includes(dinner)) {
       allergensOffered.push(dinner);
     }
-    var mealPlanRow = document.createElement("tr");
-    mealPlanRow.innerHTML = "<td>" + (day + 1) + "</td><td>" + breakfast + "</td><td>" + lunch + "</td><td>" + dinner + "</td><td>" + allergensOffered.join(", ") + "</td>";
+    
+    // Add a row to the meal plan table.
+    const mealPlanRow = document.createElement("tr");
+    mealPlanRow.innerHTML = `
+      <td>${day + 1}</td>
+      <td>${breakfast}</td>
+      <td>${lunch}</td>
+      <td>${dinner}</td>
+      <td>${allergensOffered.join(", ")}</td>
+    `;
     mealPlanTable.appendChild(mealPlanRow);
   }
 }
 
-var generateMealPlanButton = document.getElementById("generate-meal-plan-button");
+// Add an event listener to the button.
 generateMealPlanButton.addEventListener("click", generateMealPlan);

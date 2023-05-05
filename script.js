067846b020823baee2e069dfc78dd5ee07576140
Ben Goldberg <ben@goby.co>
2023-05-05 11:05:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
-// Get references to the HTML elements we need.
-var mealPlanTable = document.getElementById("meal-plan-table");
-var generateMealPlanButton = document.getElementById("generateMealPlanButton");
+function generateMealPlan() {
+  var safeFoods = [
+    "Banana", "Avocado", "Oatmeal", "Sweet potato", "Butternut squash", "Chicken",
+    "Salmon", "Tofu", "Eggs", "Yogurt", "Beef", "Lentils", "Quinoa", "Brown rice",
+    "Pasta", "Peanut butter", "Bread", "Apples", "Oranges", "Berries", "Green beans",
+    "Carrots", "Zucchini", "Squash", "Peas", "Potatoes", "Tomatoes", "Onions",
+    "Garlic", "Cucumbers", "Celery", "Kale", "Spinach", "Broccoli", "Cauliflower",
+    "Sweet potatoes", "Pumpkin", "Watermelon", "Cantaloupe", "Honeydew", "Grapefruit",
+    "Mango", "Papaya", "Apricots", "Peaches", "Nectarines"
+  ];
+  var allergens = ["Peanuts", "Tree nuts", "Eggs", "Milk", "Fish", "Shellfish", "Soy", "Wheat"];
+  var mealPlanTable = document.getElementById("meal-plan-table").getElementsByTagName('tbody')[0];
+  mealPlanTable.innerHTML = '';
+  for (var day = 0; day < 7; day++) {
+    var breakfast = safeFoods[Math.floor(Math.random() * safeFoods.length)];
+    var lunch = safeFoods[Math.floor(Math.random() * safeFoods.length)];
+    var dinner = safeFoods[Math.floor(Math.random() * safeFoods.length)];
+    var allergensOffered = [];
+    if (allergens.includes(breakfast)) {
+      allergensOffered.push(breakfast);
+    }
+    if (allergens.includes(lunch)) {
+      allergensOffered.push(lunch);
+    }
+    if (allergens.includes(dinner)) {
+      allergensOffered.push(dinner);
+    }
+    var mealPlanRow = document.createElement("tr");
+    mealPlanRow.innerHTML = "<td>" + (day + 1) + "</td><td>" + breakfast + "</td><td>" + lunch + "</td><td>" + dinner + "</td><td>" + allergensOffered.join(", ") + "</td>";
+    mealPlanTable.appendChild(mealPlanRow);
+  }
+}
 
-// Define the list of safe foods and allergens.
-var safeFoods = ["Banana", "Avocado", "Oatmeal", "Sweet potato", "Butternut squash", "Chicken", "Salmon", "Tofu", "Eggs", "Yogurt", "Beef", "Lentils", "Quinoa", "Brown rice", "Pasta", "Peanut butter", "Bread", "Apples", "Oranges", "Bananas", "Berries", "Green beans", "Carrots", "Zucchini", "Squash", "Peas", "Potatoes", "Tomatoes", "Onions", "Garlic", "Cucumbers", "Celery", "Kale", "Spinach", "Broccoli", "Cauliflower", "Sweet potatoes", "Pumpkin", "Watermelon", "Cantaloupe",
+var generateMealPlanButton = document.getElementById("generate-meal-plan-button");
+generateMealPlanButton.addEventListener("click", generateMealPlan);
